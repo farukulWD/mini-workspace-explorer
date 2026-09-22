e1ca85a0a4d39ca9b55fe27089a4c0bbdad52169
farukulWD <farukgb1999@gmail.com>
2026-09-22 11:52:12 +0600

    Project Init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Project init</h1>
+    </main>
+  );
+}
